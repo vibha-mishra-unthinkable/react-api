a37203c8e27ae34eb99917d368f4b3fff4068fcf
vibha-mishra-unthinkable <vibha.mishra@unthinkable.co>
2022-01-14 16:25:04 +0530

    added loader and error message

--- src/Api.js
+++ src/Api.js
@@ -1,36 +1,48 @@
 import axios from "axios";
 import { useState } from "react";
 export default function App() {
-  const [users, setUsers] = useState([]);
-  const fetchData = (e) => {
-    axios({
-      method: "GET",
-      url: "https://reqres.in/api/users?page=2",
-      headers: {
-        "content-type": "application/octet-stream",
-        "x-rapidapi-host": "example.com",
-        "x-rapidapi-key": process.env.RAPIDAPI_KEY,
-      },
-      params: {
-        search: "parameter",
-      },
-    })
+  const [state, setState] = useState([]);
+  const [error, setError] = useState("");
+  const [isErr, setIsErr] = useState(false);
+  const [isFetched, setIsFetched] = useState(false);
+
+  const fetchData = async (e) => {
+    setIsFetched(true);
+    await axios
+      .get("https://reqres.in/api/users?page=2")
       .then((res) => {
-        console.log(res.data.data);
-        setUsers(res.data.data);
+        console.log(res, "res");
+        if (res.data) {
+          setIsFetched(false);
+          setState(res.data.data);
+        } else {
+          setIsFetched(false);
+          setError("An error occurred-Data doesn't exist");
+          setIsErr(true);
+        }
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        setIsFetched(false);
+        setError("An error occurred-couldn't fetch the data");
+        setIsErr(true);
+      });
   };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <button onClick={(e) => fetchData(e)}>Click here</button>
+      <button onClick={fetchData}>Click here</button>
       <div>
-        {users.map((item) => (
-          <div
-            key={item.id}
-          >{`${item.id}- ${item.first_name} ${item.last_name}`}</div>
-        ))}
+        {isFetched ? (
+          <h1>Loading...</h1>
+        ) : isErr ? (
+          <h3>{error}</h3>
+        ) : (
+          state.map((item) => (
+            <div
+              key={item.id}
+            >{`${item.id}- ${item.first_name} ${item.last_name}`}</div>
+          ))
+        )}
       </div>
     </div>
   );
